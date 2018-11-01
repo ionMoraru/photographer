7abac837c956fd9fe22bce7a8eb728b2dfaa1a2e
Ion <ion.moraru.c@thalesdigital.io>
2018-11-01 14:41:07 +0100

    Updates

---
+++ precache-manifest.baddcdca71f6fed646316bf072017551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ca62fee004606ab0b96",
+    "url": "/photographer/static/js/runtime~main.6ca62fee.js"
+  },
+  {
+    "revision": "e0293289162a46f05326",
+    "url": "/photographer/static/js/main.e0293289.chunk.js"
+  },
+  {
+    "revision": "0efd217b9c06013f6feb",
+    "url": "/photographer/static/js/1.0efd217b.chunk.js"
+  },
+  {
+    "revision": "e0293289162a46f05326",
+    "url": "/photographer/static/css/main.ec246e92.chunk.css"
+  },
+  {
+    "revision": "0efd217b9c06013f6feb",
+    "url": "/photographer/static/css/1.658846a7.chunk.css"
+  },
+  {
+    "revision": "69365f4114a2a0d6f99c16c0db35a518",
+    "url": "/photographer/index.html"
+  }
+];
